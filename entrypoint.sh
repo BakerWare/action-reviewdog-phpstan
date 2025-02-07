ef095601d7cba7fb1aebc8b37385694d17421caf
Leah Venema <leah.venema@bakerware.nl>
2025-02-07 13:41:49 +0100

    PCC-780 Use PHPStan Command

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,5 +23,5 @@ if [ -n "${INPUT_TARGET_DIRECTORY}" ]; then
     TARGET_DIRECTORY="${INPUT_TARGET_DIRECTORY}"
 fi
 
-php /composer/vendor/phpstan/phpstan/phpstan.phar analyse ${TARGET_DIRECTORY} ${OPTION_LEVEL} --memory-limit 1G --error-format=raw ${INPUT_ARGS} |
+phpstan analyse ${TARGET_DIRECTORY} ${OPTION_LEVEL} --memory-limit 1G --error-format=raw ${INPUT_ARGS} |
     reviewdog -tee -name=PHPStan -f=phpstan -reporter=${INPUT_REPORTER} -fail-on-error=${INPUT_FAIL_ON_ERROR} -level=${INPUT_LEVEL} -diff='git diff'
